b685956eac761b85978b2eb7d95dd6b6804885cc
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-05-09 08:21:17 -0400

    Angular Material

---
+++ angular/src/app/material-components/calendar/calendar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent implements OnInit {
+  minDate: Date;
+  maxDate: Date;
+  constructor() {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 20, 0, 1);
+    this.maxDate = new Date(currentYear + 1, 11, 31);
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
