3de8e5cea96ead6f0a1a1ad1ba7bb9de132b7c4a
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-05-15 08:54:09 -0400

    http call ... however testing needs to be fixed

--- angular/src/app/navpages/home/home.component.spec.ts
+++ angular/src/app/navpages/home/home.component.spec.ts
@@ -1,25 +1,47 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {HomeComponent} from './home.component';
+import {DataService} from './data.service';
 
-import { HomeComponent } from './home.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  let dataServiceResult;
+
+  const dataServiceStub = jasmine.createSpyObj('DataService',
+    ['getConfig']);
+
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [HomeComponent],
+      providers: [{provide: DataService, useValue: dataServiceStub}],
+      imports: [
+        HttpClientTestingModule
+
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    dataServiceResult = TestBed.inject(DataService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get called', () => {
+
+    component.populateData()
+   // expect(dataServiceResult.getConfig.calls.count()).toBeGreaterThan(-1);
+  })
+
 });
